9743021f06bd08b965e3fdcb0d490b98db8754c7
Zach Ruiz <powersbooth10@gmail.com>
2022-06-28 11:11:53 -0500

    commit tree 66c04673644299a625016c148f776e565bd3e7af

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+
+destructivelyAppendCat;
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+
+destructivelyPrependCat;
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop();
+};
+
+destructivelyRemoveLastCat;
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift();
+};
+
+destructivelyRemoveLastCat;
+
+function appendCat(name) {
+     const newCats = [...cats,name,];
+     return newCats;
+};
+
+appendCat;
+
+function prependCat(name) {
+    const newCats = [name, ...cats,];
+    return newCats;
+};
+
+prependCat;
+
+function removeLastCat() {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+};
+
+removeLastCat;
+
+function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+}
+
+removeFirstCat;
